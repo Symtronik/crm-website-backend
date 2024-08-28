96b22102093c129edaa48f7274c0e57c7eb3de1a
staz <m.kobialka@healthpharm.pl>
2024-08-28 15:51:57 +0200

    Replace url postgresql

--- app/src/config/database.py
+++ app/src/config/database.py
@@ -6,8 +6,11 @@ from app.src.config.settings import get_settings
 
 settings = get_settings()
 
+if settings.DATABASE_URL.startswith('postgres://'):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace('postgres://', 'postgresql://')
+
 engine = create_engine(
-    url=settings.POSTGRES_DATABASE_URL,
+    url=settings.DATABASE_URL,
     pool_pre_ping=True,
     pool_size=100,
     max_overflow=50,
--- app/src/config/settings.py
+++ app/src/config/settings.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     HASHING_ALGORITHM: str = os.environ.get("HASHING_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
     # DB Settings
-    POSTGRES_DATABASE_URL: str = os.environ.get("POSTGRES_DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     class Config:
 
@@ -23,5 +23,5 @@ def get_settings():
     settings = Settings()
     # Debugowanie
     print(f"SECRET_KEY: {settings.SECRET_KEY}")
-    print(f"DATABASE_URL: {settings.POSTGRES_DATABASE_URL}")
+    print(f"DATABASE_URL: {settings.DATABASE_URL}")
     return settings

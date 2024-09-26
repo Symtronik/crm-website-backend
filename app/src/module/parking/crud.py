d5e8528f01921a7c1459a1389c6f07aa59a5683b
staz <m.kobialka@healthpharm.pl>
2024-09-26 19:32:12 +0200

    change

--- app/src/module/parking/crud.py
+++ app/src/module/parking/crud.py
@@ -1,17 +1,14 @@
 from . import models, schemas
-from sqlalchemy.orm import Session
-from datetime import datetime
 
-def add_parking(db: Session, parking: schemas.ParkingCreate, client_ip: str):
-    db_parking = models.Parking(
-        **parking.dict(),
-        created_at=datetime.now(),
-        client_ip=client_ip
-    )
+from sqlalchemy.orm import Session
+
+
+def add_parking(db:Session, parking: schemas.ParkingCreate):
+    db_parking = models.Parking(**parking.dict())
     db.add(db_parking)
     db.commit()
     db.refresh(db_parking)
     return db_parking
 
-def get_parking(db: Session, skip: int = 0, limit: int = 100):
+def get_parking(db:Session, skip: int=0, limit: int = 100):
     return db.query(models.Parking).offset(skip).limit(limit).all()

adba19dc0840e364ea7c4a3ff7cb81d896a9ee22
staz <m.kobialka@healthpharm.pl>
2024-09-26 17:48:28 +0200

    Change models parking

--- app/src/module/parking/models.py
+++ app/src/module/parking/models.py
@@ -1,15 +1,30 @@
 from ...config.database import DBBase, engine
-from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy import Column, Integer, String, Boolean, Float, Date, Time
 from sqlalchemy.orm import relationship
 
+STATUS_BOOKED = 0
+STATUS_IN_PARKING = 1
+STATUS_LEFT = 2
+STATUS_CANCEL = 3
+
 class Parking(DBBase):
-    __tablename__: str = 'parking'
+    __tablename__ = 'parking'
 
     id = Column(Integer, primary_key=True, index=True)
-    registation_number = Column(String(20), unique=True, index=True)
-    surname = Column(String(50))
+    registration_number = Column(String(20))
+    surname = Column(String(50), index=True)
     name = Column(String(50))
-    email = Column(String(50), unique=True, index=True)
+    email = Column(String(50), index=True)
+    phone = Column(String(15))
+    departure_date = Column(Date)
+    departure_fly_number= Column(String(20), index=True)
+    return_date = Column(Date)
+    departure_time = Column(Time)
+    return_time = Column(Time)
+    return_fly_number = Column(String(20), index=True)
+    status = Column(Integer, default=STATUS_BOOKED)
+    parking_number = Column(String(20))
+
 
 
 Parking.metadata.create_all(bind=engine)
